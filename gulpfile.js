c61edd8bd2c61c388dd9676f2f65197cfb0e0b16
exoriri <lavrikrom2@gmail.com>
2020-02-13 15:10:59 +0300

    fix(*): running server

--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,29 @@ gulp.task('css', () => {
         cssnano()
     ];
 
-    return gulp.src('./src/*.css')
+    return gulp.src('./src/**/.css')
         .pipe(postcss(plugins))
-        .pipe(gulp.dest('./dest'));
-});
-
-gulp.task('pug', () => {
-    return gulp.src('./src/*pug')
-        .pipe(pug())
         .pipe(gulp.dest('./dest'))
 });
 
-gulp.task('serve', gulp.series('css', 'pug'), () => {
+gulp.task('pug', () => {
+    return gulp.src('./src/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('serve', () => {
     browserSync.init({
         server: {
-            baseDir: './src'
-        }
+            baseDir: './dest/views/',
+            index: "index.html"
+        },
+        port: 3000,
     });
 
-    gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('src/views/.pug', ['pug']);
+    gulp.watch('src/css/**/*.css', gulp.series('css'));
+    gulp.watch('src/views/**/*.pug', gulp.series('pug'));
+    gulp.watch('./dest/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.series('serve'));
